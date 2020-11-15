ecb77586534e60c6759faa190670287cdb40765a
stacky151 <68614769+stacky151@users.noreply.github.com>
2020-11-15 16:17:21 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,19 +5,23 @@ module.exports = {
     name: "invite",
     description: "To add/invite the bot to your server",
     usage: "[invite]",
-    aliases: ["inv"],
+    aliases: ["inv"]
   },
 
-  run: async function (client, message, args) {
-    
+  run: async function(client, message, args) {
     //set the permissions id here (https://discordapi.com/permissions.html)
     var permissions = 37080128;
-    
+
     let invite = new MessageEmbed()
-    .setTitle(`Invite ${client.user.username}`)
-    .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
-    .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot`)
-    .setColor("BLUE")
+      .setTitle(`Invite ${client.user.username}`)
+      .setDescription(
+        `Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`
+      )
+      .setURL(
+        `https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot`
+      )
+      .setColor("BLUE")
+      .setFooter("Commands by 𝒮𝓂𝑜𝓀𝒾𝓃𝑔𝒲𝒾𝓃𝓉𝑒𝓇#1425");
     return message.channel.send(invite);
-  },
+  }
 };
